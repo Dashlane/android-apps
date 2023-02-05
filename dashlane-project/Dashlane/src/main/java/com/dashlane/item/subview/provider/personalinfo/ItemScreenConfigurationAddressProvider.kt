bc662ddcf3b924afad670a7a759e298c6a931f60
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-02-04 19:57:10 -0500

    spelling: recipient
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- dashlane-project/Dashlane/src/main/java/com/dashlane/item/subview/provider/personalinfo/ItemScreenConfigurationAddressProvider.kt
+++ dashlane-project/Dashlane/src/main/java/com/dashlane/item/subview/provider/personalinfo/ItemScreenConfigurationAddressProvider.kt
@@ -263,7 +263,7 @@ class ItemScreenConfigurationAddressProvider(
         item: VaultItem<SyncObject.Address>
     ): ItemSubView<*>? {
         return subViewFactory.createSubViewString(
-            context.getString(R.string.address_hint_receipent),
+            context.getString(R.string.address_hint_recipient),
             item.syncObject.receiver,
             false,
             VaultItem<*>::copyForUpdatedReceiver
